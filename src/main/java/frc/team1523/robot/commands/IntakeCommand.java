efaca5db2b6e04b646a711a90ab37424bebad8d5
Noskcaj <kcajdev@gmail.com>
2018-09-01 15:32:10 -0400

    Use utility square func

--- src/main/java/frc/team1523/robot/commands/IntakeCommand.java
+++ src/main/java/frc/team1523/robot/commands/IntakeCommand.java
@@ -3,6 +3,7 @@ package frc.team1523.robot.commands;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.team1523.robot.Robot;
+import frc.team1523.robot.Utilities;
 
 import static frc.team1523.robot.Constants.INTAKE_FULL_SPEED;
 
@@ -23,19 +24,10 @@ public class IntakeCommand extends Command {
         } else {
             double speed = Robot.oi.xbox.getY(GenericHID.Hand.kLeft);
             if (Math.abs(speed) >= 0.03) {
-                Robot.intake.setSpeed(squared(speed));
+                Robot.intake.setSpeed(Utilities.signedSqrt(speed));
             } else {
                 Robot.intake.setSpeed(0);
             }
-        }
-    }
-
-    private double squared(double input) {
-        // Make sure to keep the sign so the direction you input is not changed by squaring it
-        if (input > 0) {
-            return input * input;
-        } else {
-            return -(input * input);
         }
     }
 
